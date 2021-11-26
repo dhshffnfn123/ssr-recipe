90926b4c45bd000f41a9a5476d872ee84a778681
B H An <anbyunghoon0107@gamil.com>
2021-11-26 10:48:36 +0900

    Sagamiddleware 적용

--- src/index.server.js
+++ src/index.server.js
@@ -8,7 +8,8 @@ import fs from 'fs';
 import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
-import rootReducer from "./modules";
+import createSagaMiddleware from 'redux-saga';
+import rootReducer, { rootSaga } from "./modules";
 import PreloadContext from "./lib/PreloadContext";
 
 // asset-manifest.json에서 파일 경로들을 조회한다.
@@ -56,7 +57,11 @@ const app = express();
 const serverRender = async (req, res, next) => {
     // 이 함수는 404가 떠야하는 상황에 404를 띄우지 않고 서버 사이드 렌더링을 해준다.
     const context ={};
-    const store = createStore(rootReducer, applyMiddleware(thunk));
+    const sagaMiddleware = createSagaMiddleware();
+
+    const store = createStore(rootReducer, applyMiddleware(thunk, sagaMiddleware));
+
+    sagaMiddleware.run(rootSaga);
 
     const preloadContext = {
         done: false,
